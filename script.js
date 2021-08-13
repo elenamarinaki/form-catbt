c6ee83937b06d203d8e3bd71000e5e3f48205360
Elena <rhapsodyenmauve@gmail.com>
2021-08-13 21:27:51 +0100

    uk phone numbers validation

--- script.js
+++ script.js
@@ -27,15 +27,27 @@ function validateName() {
 
 // ------------------------- EMAIL VALIDATION
 function validateEmail() {
-  const re =
+  const reEmail =
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  if (!re.test(email.value)) {
+  if (!reEmail.test(email.value)) {
     email.classList.add('highlight');
     warnings[1].classList.remove('hide');
   } else {
     email.classList.remove('highlight');
     warnings[1].classList.add('hide');
+  }
+}
+
+function validatePhone() {
+  const rePhone = /^((\(\+44\))|0)[0-9]{10}/;
+
+  if (!rePhone.test(phone.value)) {
+    phone.classList.add('highlight');
+    warnings[3].classList.remove('hide');
+  } else {
+    phone.classList.remove('highlight');
+    warnings[3].classList.add('hide');
   }
 }
 
@@ -56,4 +68,5 @@ function submitForm() {
 window.addEventListener('load', init);
 name.addEventListener('input', validateName);
 email.addEventListener('input', validateEmail);
+phone.addEventListener('input', validatePhone);
 submitData.addEventListener('click', submitForm);
